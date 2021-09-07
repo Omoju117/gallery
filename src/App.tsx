a3dfd7d9a076b0e1ec4647566afbd0567c51c876
kazukiendo <ka.endo@nagatake.co.jp>
2021-09-07 10:56:12 +0900

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App:VFC = () => (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          source cloning successfully!
         </p>
         <a
           className="App-link"
